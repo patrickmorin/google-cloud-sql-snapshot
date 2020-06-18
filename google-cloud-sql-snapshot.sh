c817f7399a81368a84701442e6f3521688be814a
Patrick Morin <morin@esokia.com>
2020-06-18 16:12:56 +0400

    Switch to array of databases for multiple snapshot

--- google-cloud-sql-snapshot.sh
+++ google-cloud-sql-snapshot.sh
@@ -6,14 +6,17 @@ if [ -z "${DAYS_RETENTION}" ]; then
 fi
 
 BUCKET_NAME="BUCKET_NAME"
-DATABASE="DATABASE_NAME"
+DATABASE=( "DATABASE_NAME_1" "DATABASE_NAME_2" )
 SQL_INSTANCE="SQL_INSTANCE"
 
 # Author: Patrick Morin, Esokia
 # authentification with service account
 gcloud auth activate-service-account --key-file=/path/to/serviceaccount.json
-# export database from instance and create a snapshot
-gcloud sql export sql "${SQL_INSTANCE}" gs://"${BUCKET_NAME}"/sqldumpfile-"$(date '+%Y-%m-%d-%s')".sql.gz --database "${DATABASE}"
+# export databases from instance and create a snapshot
+for db in "${DATABASE[@]}"
+do
+  gcloud sql export sql "${SQL_INSTANCE}" gs://"${BUCKET_NAME}"/sqldumpfile-"${db}"-"$(date '+%Y-%m-%d-%s')".sql.gz --database "${db}"
+done
 #
 # snapshots are incremental and dont need to be deleted, this script deletes them after n days
 #
@@ -22,5 +25,8 @@ if [[ $(uname) == "Linux" ]]; then
 else
   from_date=$(date -v -${DAYS_RETENTION}d "+%Y-%m-%d")
 fi
-gsutil rm -r gs://"${BUCKET_NAME}"/sqldumpfile-"${from_date}"*
+for db in "${DATABASE[@]}"
+do
+  gsutil rm -r gs://"${BUCKET_NAME}"/sqldumpfile-"${db}"-"${from_date}"*
+done
 #
